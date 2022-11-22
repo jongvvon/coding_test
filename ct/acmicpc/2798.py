423dca2b0ab871527106e5a2b8fff6db6687c0a9
jongvvon <jwjang7024@gmail.com>
2022-11-22 18:59:23 +0900

    221122

--- ct/acmicpc/2798.py
+++ ct/acmicpc/2798.py
@@ -1,8 +1,13 @@
 N, M = map(int, input().split())
 card = list(map(int, input().split()))
+nearNum = 0
+temp = {}
+for i in range(0, len(card)-2):
+    for j in range(i+1, len(card)-1):
+        for k in range(j+1, len(card)):
+            nearNum = card[i]+card[j]+card[k]
+            if nearNum <= M:
+                temp[nearNum] = M - nearNum
 
-temp = 0
-
-for i in range(N):
-    for j in range(i, i+3):
-        temp += card[i]
+print(temp)
+print(min(temp, key=temp.get))
