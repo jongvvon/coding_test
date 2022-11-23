5729de27a11d19de6c2aa9e7bc4ec4d6dfa62cac
jongvvon <jwjang7024@gmail.com>
2022-11-23 18:11:13 +0900

    221123

--- ct/acmicpc/12865.py
+++ ct/acmicpc/12865.py
@@ -46,10 +46,10 @@ for _ in range(N):
     w, v = map(int, input().split())
     items.append((w, v))
 
-    for item in items:
-        w, v = item
+for item in items:
+    w, v = item
 
-        for i in range(K, w-1, -1):
-            sum_value[i] = max(sum_value[i], sum_value[i-w]+v)
+    for i in range(K, w-1, -1):
+        sum_value[i] = max(sum_value[i], sum_value[i-w]+v)
 
 print(sum_value[-1])
