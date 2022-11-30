c5cb2ab76dd9eebd660b80d28214a2118b7da91d
jongvvon <jwjang7024@gmail.com>
2022-11-30 09:15:08 +0900

    221130 solved

--- ct/acmicpc/week1_mathmatics/11653.py
+++ ct/acmicpc/week1_mathmatics/11653.py
@@ -1,13 +1,7 @@
-import math
-
-n = 100000
-array = [True for i in range(n+1)]
-array[0], array[1] = False, False
-
-for i in range(2, int(math.sqrt(n)) + 1):
-    if array[i]:
-        j = 2
-        while i * j <= n:
-            array[i * j] = False
-            j += 1
-
+n = int(input())
+for i in range(2, n + 1):
+    while n % i == 0:
+        print(i)
+        n //= i
+    if n == 1:
+        break
