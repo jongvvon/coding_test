4131091442d3fb95bb80078a675289d7e0807f31
qkdkad <dnjs_24@naver.com>
2022-12-03 16:51:59 +0900

    221203 solved

--- ct/acmicpc/week1_mathmatics/2981.py
+++ ct/acmicpc/week1_mathmatics/2981.py
@@ -1,13 +1,6 @@
 # 유클리드 호제법
 # num of wrote numbers in paper
-N = int(input())
-nums = []
-for _ in range(N):
-    nums.append(int(input()))
-
-temp = []
-for i in range(N-1):
-    temp.append(nums[i+1] - nums[i])
+import math
 
 
 def GCD(x, y):
@@ -16,9 +9,24 @@ def GCD(x, y):
     return x
 
 
-gcd_array = []
-for j in range(len(temp)):
-    gcd_array[i] = GCD(temp[i], i)
+N = int(input())
+nums = sorted([int(input()) for _ in range(N)])
+
+temp = []
+for i in range(N-1):
+    temp.append(nums[i+1] - nums[i])
+
+gcd_num = temp[0]
+for j in range(1, len(temp)):
+    gcd_num = GCD(gcd_num, temp[i])
+
+result = set()
+for k in range(2, int(math.sqrt(gcd_num)) + 1):
+    if gcd_num % k == 0:
+        result.add(k)
+        result.add(gcd_num // k)
+result.add(gcd_num)
+print(*sorted(list(result)))
 
 # min(nums)가 10억이 될 수 있음 -> 시간 초과
 # for i in range(2, min(nums)+1):
