1ebade1d4f000ba1f602dc44be05939be7179a05
jongvvon <jwjang7024@gmail.com>
2022-12-04 16:13:03 +0900

    221204 solved

---
+++ ct/acmicpc/week1_mathmatics/17087.py
@@ -0,0 +1,17 @@
+# N is num of brother, S is current location
+# locations are brother locations
+N, S = map(int, input().split())
+locations = list(map(int, input().split()))
+locations.sort()
+
+if min(locations) == max(locations) == S:
+    print(0)
+elif locations[0] >= S:
+    print(max(locations) - S)
+elif locations[-1] <= S:
+    print(min(locations) - S)
+else:
+    if max(locations) - S > S - min(locations):
+        print(S-min(locations))
+    else:
+        print(max(locations)-S)
--- ct/acmicpc/week1_mathmatics/2824.py
+++ ct/acmicpc/week1_mathmatics/2824.py
@@ -3,3 +3,5 @@ N_nums = list(map(int, input().split()))
 M = int(input())
 M_nums = list(map(int, input().split()))
 
+# N_nums 는 N의 약수, M 또한 동일 -> 약수간의 최대공약수? 비교
+
