44772f76e5558c0dea5e6297061eed00e2c1b90e
jongvvon <jwjang7024@gmail.com>
2022-12-06 01:05:15 +0900

    221206 solved

--- ct/acmicpc/week1_mathmatics/17087.py
+++ ct/acmicpc/week1_mathmatics/17087.py
@@ -1,17 +1,16 @@
 # N is num of brother, S is current location
 # locations are brother locations
+import math
+
 N, S = map(int, input().split())
 locations = list(map(int, input().split()))
-locations.sort()
 
-if min(locations) == max(locations) == S:
-    print(0)
-elif locations[0] >= S:
-    print(max(locations) - S)
-elif locations[-1] <= S:
-    print(S - min(locations))
-else:
-    if max(locations) - S > S - min(locations):
-        print(S-min(locations))
-    else:
-        print(max(locations)-S)
+array = []
+for i in range(N):
+    array.append(abs(S-locations[i]))
+
+gcd = array[0]
+for j in range(1, N):
+    gcd = math.gcd(gcd, array[j])
+
+print(gcd)
