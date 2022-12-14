405e3f366a7577f0490e4679f7cf0fc1f82b7802
jongvvon <jwjang7024@gmail.com>
2022-12-14 19:34:55 +0900

    221214 solved

---
+++ ct/acmicpc/week4_dynamic_programming/11053.py
@@ -0,0 +1,11 @@
+num = int(input())
+array = list(map(int, input().split()))
+
+dp = [0] * num
+for i in range(num):
+    for j in range(i):
+        if array[i] > array[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+
+print(max(dp))
