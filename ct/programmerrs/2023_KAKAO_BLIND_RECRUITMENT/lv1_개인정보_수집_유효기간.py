2707e2baa41b69e0c8f8ffc83cf7fed05b11ff3d
jongvvon <jwjang7024@gmail.com>
2024-03-20 20:13:30 +0900

    0320

--- ct/programmerrs/2023_KAKAO_BLIND_RECRUITMENT/lv1_개인정보_수집_유효기간.py
+++ ct/programmerrs/2023_KAKAO_BLIND_RECRUITMENT/lv1_개인정보_수집_유효기간.py
@@ -26,3 +26,19 @@ privacies = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
 # terms = ["Z 3", "D 5"]
 # privacies = ["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]
 print(solution(today, terms, privacies))
+
+
+# 모든 달은 28일!
+
+def to_days(date):
+    year, month, day = map(int, date.split("."))
+    return year * 28 * 12 + month * 28 + day
+
+def solution(today, terms, privacies):
+    months = {v[0]: int(v[2:]) * 28 for v in terms}
+    today = to_days(today)
+    expire = [
+        i + 1 for i, privacy in enumerate(privacies)
+        if to_days(privacy[:-2]) + months[privacy[-1]] <= today
+    ]
+    return expire
